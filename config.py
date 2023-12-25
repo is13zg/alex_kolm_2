e865f0ef1f8b9e68a61f8367ea0bfb2b12042419
is13 <is13mozg@gmail.com>
2023-12-25 18:53:06 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+BotToken = "6432816189:AAEVg-j6_kIL9TwN_D78ArablWW93Vsuatw"
+BD_name = "alx_kol_files/databases/bonus1.db"
+Interaction_file_name = "alx_kol_files/answers/interaction.json"
+Answers_file_name = "alx_kol_files/answers/answers.json"
+Support_chat_id = -1001987964716
+Owner_id = 97875888
